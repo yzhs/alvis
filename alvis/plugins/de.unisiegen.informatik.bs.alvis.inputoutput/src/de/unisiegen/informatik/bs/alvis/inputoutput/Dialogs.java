feb75715262049c26f6769a9d023673fd325e184
groth <groth@57b514eb-d8df-4872-b74f-14c3cc28987e>
2011-05-06 09:33:27 +0000

    @new InputOutput plugin with xstream serialisation comming soon dialogs
    git-svn-id: https://vcs.informatik.uni-siegen.de/svn/algvistool/trunk@426 57b514eb-d8df-4872-b74f-14c3cc28987e

---
+++ alvis/plugins/de.unisiegen.informatik.bs.alvis.inputoutput/src/de/unisiegen/informatik/bs/alvis/inputoutput/Dialogs.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package de.unisiegen.informatik.bs.alvis.inputoutput;
+
+import org.eclipse.swt.widgets.FileDialog;
+
+/**
+ * Collection of usefull dialogs.
+ * @author simon
+ *
+ */
+public class Dialogs {
+
+	public void getFileChooser() {
+//		FileDialog dialog = new FileDialog (shell, SWT.SAVE);
+//		String [] filterNames = new String [] {"Image Files", "All Files (*)"};
+//		String [] filterExtensions = new String [] {"*.gif;*.png;*.xpm;*.jpg;*.jpeg;*.tiff", "*"};
+//		String filterPath = "/";
+//		String platform = SWT.getPlatform();
+//		if (platform.equals("win32") || platform.equals("wpf")) {
+//			filterNames = new String [] {"Image Files", "All Files (*.*)"};
+//			filterExtensions = new String [] {"*.gif;*.png;*.bmp;*.jpg;*.jpeg;*.tiff", "*.*"};
+//			filterPath = "c:\\";
+//		}
+//		dialog.setFilterNames (filterNames);
+//		dialog.setFilterExtensions (filterExtensions);
+//		dialog.setFilterPath (filterPath);
+//		dialog.setFileName ("myfile");
+	}
+}
