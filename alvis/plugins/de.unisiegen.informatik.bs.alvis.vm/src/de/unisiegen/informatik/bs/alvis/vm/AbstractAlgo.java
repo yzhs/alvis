34b338997ced7cabf0c9b0176300be5956fc27b4
dingel <dingel@57b514eb-d8df-4872-b74f-14c3cc28987e>
2011-05-07 15:44:17 +0000

    - adding vm code, currently referencing prmitive, should be changed to primitive.datatype - currently disabled algo code, will be moved to test code
    git-svn-id: https://vcs.informatik.uni-siegen.de/svn/algvistool/trunk@449 57b514eb-d8df-4872-b74f-14c3cc28987e

---
+++ alvis/plugins/de.unisiegen.informatik.bs.alvis.vm/src/de/unisiegen/informatik/bs/alvis/vm/AbstractAlgo.java
@@ -0,0 +1,39 @@
+package de.unisiegen.informatik.bs.alvis.vm;
+
+import java.util.ArrayList;
+
+import de.unisiegen.informatik.bs.alvis.primitives.Datatype;
+
+public abstract class AbstractAlgo extends Thread {
+	protected BPListener bplisten;
+	protected DPListener dplisten;
+	public boolean onBreak;
+
+	public void addBPListener(BPListener wantToListen) {
+		bplisten = wantToListen;
+	}
+
+	public void addDPListener(DPListener wantToListen) {
+		dplisten = wantToListen;
+	}
+
+	protected void reachedBreakPoint(int BPNr) {
+		onBreak = true;
+		bplisten.onBreakPoint(BPNr);
+
+		synchronized (this) {
+			while (onBreak) {
+				try {
+					wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	abstract public ArrayList<Datatype> getRunningRef();
+
+	abstract public void run();
+
+}
---
+++ alvis/plugins/de.unisiegen.informatik.bs.alvis.vm/src/de/unisiegen/informatik/bs/alvis/vm/BPListener.java
@@ -0,0 +1,5 @@
+package de.unisiegen.informatik.bs.alvis.vm;
+
+public interface BPListener {
+	void onBreakPoint(int BreakPointNumber);
+}
