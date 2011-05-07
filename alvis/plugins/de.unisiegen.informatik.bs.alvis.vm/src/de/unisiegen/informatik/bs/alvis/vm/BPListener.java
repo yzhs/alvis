919f4eedfeb33d117ceaadd9b2cbb3cf192eeb14
dingel <dingel@57b514eb-d8df-4872-b74f-14c3cc28987e>
2011-05-07 20:54:36 +0000

    project created with wrong settings, have to create fresh
    git-svn-id: https://vcs.informatik.uni-siegen.de/svn/algvistool/trunk@460 57b514eb-d8df-4872-b74f-14c3cc28987e

--- alvis/plugins/de.unisiegen.informatik.bs.alvis.vm/src/de/unisiegen/informatik/bs/alvis/vm/BPListener.java
+++
@@ -1,5 +0,0 @@
-package de.unisiegen.informatik.bs.alvis.vm;
-
-public interface BPListener {
-	void onBreakPoint(int BreakPointNumber);
-}
--- alvis/plugins/de.unisiegen.informatik.bs.alvis.vm/src/de/unisiegen/informatik/bs/alvis/vm/DPListener.java
+++
@@ -1,7 +0,0 @@
-package de.unisiegen.informatik.bs.alvis.vm;
-
-import de.unisiegen.informatik.bs.alvis.primitives.SortableCollection;
-
-public interface DPListener {
-	void onDecisionPoint(int DPNr, SortableCollection toSort);
-}
