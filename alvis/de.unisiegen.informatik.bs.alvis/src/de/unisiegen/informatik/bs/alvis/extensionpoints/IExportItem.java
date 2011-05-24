4f7d28f2f3c3c07efb6506cd5e10aeea70c1e48a
weiler <weiler@57b514eb-d8df-4872-b74f-14c3cc28987e>
2011-05-24 22:06:09 +0000

    export preparation
    git-svn-id: https://vcs.informatik.uni-siegen.de/svn/algvistool/trunk@561 57b514eb-d8df-4872-b74f-14c3cc28987e

---
+++ alvis/de.unisiegen.informatik.bs.alvis/src/de/unisiegen/informatik/bs/alvis/extensionpoints/IExportItem.java
@@ -0,0 +1,32 @@
+package de.unisiegen.informatik.bs.alvis.extensionpoints;
+
+
+import org.eclipse.swt.graphics.Image;
+
+/**
+ * 
+ * @author Frank Weiler
+ * @description the export object interface. every exportable object (i.e.
+ *              text-> source code, images-> graph, etc) needs to be implement
+ *              this
+ */
+public interface IExportItem {
+
+	// TODO not only a string should be returned, but formated strings with font
+	// styles etc
+	/**
+	 * returns source code for export
+	 * 
+	 * @return source code for export, null if object does not contain any
+	 *         source code (e.g. graph files)
+	 */
+	public String getSourceCode();
+
+	/**
+	 * returns image for export
+	 * 
+	 * @return image for export, null if object does not contain any image
+	 */
+	public Image getImage();
+
+}
