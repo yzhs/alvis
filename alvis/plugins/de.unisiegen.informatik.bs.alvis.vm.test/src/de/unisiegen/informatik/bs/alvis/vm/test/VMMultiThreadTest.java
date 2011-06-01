fd5c70db0e0138f283a4ff9ee98fc280c68be1c3
dingel <dingel@57b514eb-d8df-4872-b74f-14c3cc28987e>
2011-06-01 11:15:57 +0000

    inital multithread test
    git-svn-id: https://vcs.informatik.uni-siegen.de/svn/algvistool/trunk@593 57b514eb-d8df-4872-b74f-14c3cc28987e

---
+++ alvis/plugins/de.unisiegen.informatik.bs.alvis.vm.test/src/de/unisiegen/informatik/bs/alvis/vm/test/VMMultiThreadTest.java
@@ -0,0 +1,27 @@
+package de.unisiegen.informatik.bs.alvis.vm.test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import junit.framework.Assert;
+
+import org.testng.annotations.Test;
+
+import de.unisiegen.informatik.bs.alvis.primitive.datatypes.PCInteger;
+import de.unisiegen.informatik.bs.alvis.primitive.datatypes.PCObject;
+import de.unisiegen.informatik.bs.alvis.vm.BPListener;
+import de.unisiegen.informatik.bs.alvis.vm.VirtualMachine;
+
+/**
+ * 
+ * @author 
+ * 
+ */
+
+public class VMMultiThreadTest {
+	
+}
