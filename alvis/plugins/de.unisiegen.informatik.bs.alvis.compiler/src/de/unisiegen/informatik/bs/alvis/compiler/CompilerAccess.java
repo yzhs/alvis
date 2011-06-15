267501528587b72cac154b4540023313827e27dc
groth <groth@57b514eb-d8df-4872-b74f-14c3cc28987e>
2011-06-15 07:46:54 +0000

    Neugenerierung des Compilers mit erweitertem Zugriff auf RCP Funktionen.
    git-svn-id: https://vcs.informatik.uni-siegen.de/svn/algvistool/trunk@649 57b514eb-d8df-4872-b74f-14c3cc28987e

--- alvis/plugins/de.unisiegen.informatik.bs.alvis.compiler/src/de/unisiegen/informatik/bs/alvis/compiler/CompilerAccess.java
+++ alvis/plugins/de.unisiegen.informatik.bs.alvis.compiler/src/de/unisiegen/informatik/bs/alvis/compiler/CompilerAccess.java
@@ -31,18 +31,18 @@ public class CompilerAccess {
 	 */
 	public static String compileThisDummy(String code, ArrayList<PCObject> datatypes) {
 //		String javaFilePath = "-1";
-//		try {
-//			String dir = FileLocator.getBundleFile(
-//					Activator.getDefault().getBundle())
-//					.getAbsolutePath().toString();
-//////			 javaFilePath = dir+"/Algorithm.java";
-////			 javaFilePath = "Algorithm";
-//////			 javaFilePath = javaFilePath.replaceAll("\\\\", "/");
-////
-////			 System.out.println("Dir: " + dir);
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}	
+		try {
+			String dir = FileLocator.getBundleFile(
+					Activator.getDefault().getBundle())
+					.getAbsolutePath().toString();
+////			 javaFilePath = dir+"/Algorithm.java";
+//			 javaFilePath = "Algorithm";
+////			 javaFilePath = javaFilePath.replaceAll("\\\\", "/");
+//
+//			 System.out.println("Dir: " + dir);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}	
 		String javaFilePath = "Algorithm"; // TODO HARD CODED!
 		return javaFilePath;
 	}
