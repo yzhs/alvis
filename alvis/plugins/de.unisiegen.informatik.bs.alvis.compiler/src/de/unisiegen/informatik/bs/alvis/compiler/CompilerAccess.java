c3b943b48beed3f7c31e8649fe1af8e13d8a4a99
groth <groth@57b514eb-d8df-4872-b74f-14c3cc28987e>
2011-07-01 18:02:58 +0000

    Kopieren des Algorithmus.java in den workspace hinzugefuegt.
    git-svn-id: https://vcs.informatik.uni-siegen.de/svn/algvistool/trunk@749 57b514eb-d8df-4872-b74f-14c3cc28987e

--- alvis/plugins/de.unisiegen.informatik.bs.alvis.compiler/src/de/unisiegen/informatik/bs/alvis/compiler/CompilerAccess.java
+++ alvis/plugins/de.unisiegen.informatik.bs.alvis.compiler/src/de/unisiegen/informatik/bs/alvis/compiler/CompilerAccess.java
@@ -3,73 +3,127 @@
  */
 package de.unisiegen.informatik.bs.alvis.compiler;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
 import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Platform;
 
+import de.unisiegen.informatik.bs.alvis.io.files.FileCopy;
 import de.unisiegen.informatik.bs.alvis.primitive.datatypes.PCObject;
-
 
 /**
  * @author mays
- *
+ * 
  */
 public class CompilerAccess {
-	
+
 	private static CompilerAccess instance;
-	
+
 	public static CompilerAccess getDefault() {
-		if(instance == null)
+		if (instance == null)
 			instance = new CompilerAccess();
 		return instance;
 	}
-	
+
 	private ArrayList<PCObject> datatypes;
 	private ArrayList<String> datatypePackages;
-	
-	
+
 	public boolean compile(String code, ArrayList<PCObject> datatypes) {
 		// TODO Compilieren
-//		CompilerManager manager = new CompilerManager();
+		// CompilerManager manager = new CompilerManager();
 		return true;
 	}
-	
+
+	String algorithmPath = "";
+
 	/**
 	 * Returns the path to the algorithm file
+	 * 
 	 * @return
 	 * @throws IOException
 	 */
 	public String getAlgorithmPath() throws IOException {
 		String path = "";
-		path = FileLocator.getBundleFile(Activator.getDefault().getBundle()).getCanonicalPath().toString();
-		return path;
+		path = FileLocator.getBundleFile(Activator.getDefault().getBundle())
+				.getCanonicalPath().toString();
+		return algorithmPath;
 	}
-	
+
 	/**
-	 * 
-	 * @param code
-	 * @param datatypes
-	 * @return
+	 * This method copies the Dummy Algorithm file next to the PCAlgorithm file
+	 * that is written by the user.
+	 * To get the path of the created file see getAlgorithmPath().
+	 * @param pathToAlgorithm
+	 *            relative to Alvis-workspace e.g.: "project/src/Algorithm.algo"
+	 * @return Name of the Java Algorithm file
 	 */
-	public String compileThisDummy(String code, ArrayList<PCObject> datatypes) {	
+	public String compileThisDummy(String pathToAlgorithm) {
+		String SLASH = System.getProperty("file.separator");
+
+		// the path were the translated java file is.
+		String pathWhereTheJavaIs = "";
+		try {
+			pathWhereTheJavaIs = FileLocator
+					.getBundleFile(Activator.getDefault().getBundle())
+					.getCanonicalPath().toString();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		// The compiled algorithm
+		File source = new File(pathWhereTheJavaIs + SLASH + "Algorithm.java");
+
+		// Get the path to algorithm and separate path and filename
+		String[] splitedPathToAlgorithm = pathToAlgorithm.split("\\/");
+		ArrayList<String> partsOfAlgoPath = new ArrayList<String>();
+		for (String part : splitedPathToAlgorithm) {
+			partsOfAlgoPath.add(part);
+		}
+
+		// getFileName
+		String algoWorkSpaceFile = partsOfAlgoPath.remove(partsOfAlgoPath.size()-1);
+		
+		// getPath
+		String algoWorkSpacePath = "";
+		for (String part : partsOfAlgoPath) {
+			algoWorkSpacePath += part + SLASH;
+		}
+
+//		for (String st : splitedPathToAlgorithm)
+//			System.out.println(st);
+
+		// Destination 
+		File destination = new File(Platform.getInstanceLocation().getURL()
+				.getPath()
+				+ algoWorkSpacePath + "Algorithm.java");
+		algorithmPath = Platform.getInstanceLocation().getURL().getPath()
+				+ algoWorkSpacePath + SLASH;
+
+		// Copy compiled file into the workspace
+		FileCopy fileCopy = new FileCopy();
+		fileCopy.copy(source, destination);
+
+		// Still hard Coded.
 		String javaFilePath = "Algorithm"; // TODO HARD CODED!
 		return javaFilePath;
 	}
 
-	
 	/* ******************************************
-	 * The Datatypes and Packagenames
-	 * ******************************************/
+	 * The Datatypes and Packagenames *****************************************
+	 */
 	/**
-	 * @param datatypes the datatypes to set
+	 * @param datatypes
+	 *            the datatypes to set
 	 */
 	public void setDatatypes(ArrayList<PCObject> datatypes) {
 		this.datatypes = datatypes;
 	}
 
 	/**
-	 * @param datatypePackages the datatypePackages to set
+	 * @param datatypePackages
+	 *            the datatypePackages to set
 	 */
 	public void setDatatypePackages(ArrayList<String> datatypePackages) {
 		this.datatypePackages = datatypePackages;
@@ -78,12 +132,12 @@ public class CompilerAccess {
 	public void testDatatypes() {
 		System.out.println("Compiler shows its datatypes:");
 		// Hier sind alle Datentypen
-		for(PCObject obj : datatypes) {
+		for (PCObject obj : datatypes) {
 			System.out.println(obj.getClass());
 		}
 		System.out.println("Compiler shows its packages:");
 		// Hier sind alle Packagenames
-		for(String obj : datatypePackages) {
+		for (String obj : datatypePackages) {
 			System.out.println(obj);
 		}
 	}
