7daa0cf86d62a98ff1ad9b0f2cd3f581f0ed2656
boos <boos@57b514eb-d8df-4872-b74f-14c3cc28987e>
2011-09-01 00:52:00 +0000

    Added License Information to classes I worked with.
    git-svn-id: https://vcs.informatik.uni-siegen.de/svn/algvistool/trunk@1117 57b514eb-d8df-4872-b74f-14c3cc28987e

--- alvis/plugins/de.unisiegen.informatik.bs.alvis.graph/src/de/unisiegen/informatik/bs/alvis/graph/newwizards/NewGraphWizard.java
+++ alvis/plugins/de.unisiegen.informatik.bs.alvis.graph/src/de/unisiegen/informatik/bs/alvis/graph/newwizards/NewGraphWizard.java
@@ -1,6 +1,21 @@
-/**
- * Creates a new Algorithm Wizard which creates a new File with the .algo extension
+/*
+ * Copyright (c) 2011 Eduard Boos
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in the
+ * Software without restriction, including without limitation the rights to use, 
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the 
+ * Software, and to permit persons to whom the Software is furnished to do so, 
+ * subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all 
+ * copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+
 package de.unisiegen.informatik.bs.alvis.graph.newwizards;
 
 import org.eclipse.core.resources.IFile;
@@ -13,6 +28,7 @@ import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.ide.IDE;
 
 /**
+ * Creates a new Algorithm Wizard which creates a new File with the .algo extension
  * @author Eduard Boos
  *
  */
