b3147e18ccd113315541514629a83955c52d1d31
dingel <dingel@57b514eb-d8df-4872-b74f-14c3cc28987e>
2011-09-09 12:27:58 +0000

    - now also possible to set edge color - fixing bugs
    git-svn-id: https://vcs.informatik.uni-siegen.de/svn/algvistool/trunk@1139 57b514eb-d8df-4872-b74f-14c3cc28987e

--- alvis/plugins/de.unisiegen.informatik.bs.alvis.graph.datatypes/src/de/unisiegen/informatik/bs/alvis/graph/datatypes/PCEdge.java
+++ alvis/plugins/de.unisiegen.informatik.bs.alvis.graph.datatypes/src/de/unisiegen/informatik/bs/alvis/graph/datatypes/PCEdge.java
@@ -18,14 +18,17 @@
 
 package de.unisiegen.informatik.bs.alvis.graph.datatypes;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
 
 import de.unisiegen.informatik.bs.alvis.primitive.datatypes.GraphicalRepresentation;
 import de.unisiegen.informatik.bs.alvis.primitive.datatypes.PCInteger;
 import de.unisiegen.informatik.bs.alvis.primitive.datatypes.PCList;
 import de.unisiegen.informatik.bs.alvis.primitive.datatypes.PCObject;
 import de.unisiegen.informatik.bs.alvis.primitive.datatypes.PCBoolean;
+import de.unisiegen.informatik.bs.alvis.primitive.datatypes.PCString;
 
 /**
  * 
@@ -44,6 +47,7 @@ public class PCEdge extends PCObject implements Comparable<PCEdge> {
 	private PCVertex v2;
 	private PCBoolean isDirected;
 	private PCInteger weight;
+	private PCString color;
 
 	/**
 	 * Default Constructor: not directed and 0 weight
@@ -53,6 +57,10 @@ public class PCEdge extends PCObject implements Comparable<PCEdge> {
 		v2 = new PCVertex();
 		isDirected = new PCBoolean(false);
 		weight = new PCInteger(0);
+		color = new PCString("");
+		commandsforGr = new ArrayList<Stack<Object>>();
+		commandsforGr.add(new Stack<Object>());
+		commandsforGr.add(new Stack<Object>());
 	}
 
 	/**
@@ -69,6 +77,11 @@ public class PCEdge extends PCObject implements Comparable<PCEdge> {
 		isDirected = new PCBoolean(false);
 		notifyVertices();
 		weight = new PCInteger(edge.getWeight());
+		color = new PCString(edge.getColorText());
+		commandsforGr = new ArrayList<Stack<Object>>();
+		commandsforGr.add(new Stack<Object>());
+		commandsforGr.add(new Stack<Object>());
+
 	}
 
 	/**
@@ -83,6 +96,11 @@ public class PCEdge extends PCObject implements Comparable<PCEdge> {
 		isDirected = new PCBoolean(false);
 		notifyVertices();
 		weight = new PCInteger(0);
+		color = new PCString("");
+		commandsforGr = new ArrayList<Stack<Object>>();
+		commandsforGr.add(new Stack<Object>());
+		commandsforGr.add(new Stack<Object>());
+
 	}
 
 	/**
@@ -98,6 +116,10 @@ public class PCEdge extends PCObject implements Comparable<PCEdge> {
 		this.isDirected = new PCBoolean(false);
 		notifyVertices();
 		weight = new PCInteger(0);
+		color = new PCString("");
+		commandsforGr = new ArrayList<Stack<Object>>();
+		commandsforGr.add(new Stack<Object>());
+		commandsforGr.add(new Stack<Object>());
 	}
 
 	/**
@@ -129,7 +151,7 @@ public class PCEdge extends PCObject implements Comparable<PCEdge> {
 	@Override
 	public List<String> getMembers() {
 		String[] attributes = { "weight", "vertices", "startVertex",
-				"endVertex" };
+				"endVertex", "color" };
 		return Arrays.asList(attributes);
 	}
 
@@ -140,35 +162,60 @@ public class PCEdge extends PCObject implements Comparable<PCEdge> {
 		if (memberName.equals("vertices")) {
 			return this.getVertices();
 		}
-		if (memberName.equals("startVertex"))
+		if (memberName.equals("startVertex")) {
 			return this.getStartVertex();
-		if (memberName.equals("endVertex"))
+		}
+		if (memberName.equals("endVertex")) {
 			return this.getEndVertex();
+		}
+		if (memberName.equals("color")) {
+			return this.getColor();
+		}
 
 		return null;
+	}
+
+	public PCString getColor() {
+		return this.color;
 	}
 
 	public PCVertex getStartVertex() {
 		// TODO is not necessary v1
 		return this.v1;
 	}
-	
+
 	public PCVertex getEndVertex() {
 		// TODO is not necessary v1
 		return this.v2;
 	}
-	
+
 	public PCInteger getWeight() {
 		return this.weight;
 	}
 
+	public void setColor(PCString color) {
+		this.color = color;
+		if (!this.isInBatchRun) {
+			for (GraphicalRepresentation gredge : allGr) {
+				((GraphicalRepresentationEdge) gredge).setColor(color
+						.getLiteralValue());
+
+			}
+		} else {
+			commandsforGr.get(1).push(weight);
+		}
+	}
+
 	public void setWeight(PCInteger weight) {
 		this.weight = weight;
-		if (!this.isInBatchRun)
+		if (!this.isInBatchRun) {
 			for (GraphicalRepresentation gredge : allGr) {
 				((GraphicalRepresentationEdge) gredge)
 						.setWeight(((PCInteger) weight).getLiteralValue());
 			}
+		} else {
+			commandsforGr.get(0).push(weight);
+		}
 	}
 
 	@Override
@@ -202,17 +249,24 @@ public class PCEdge extends PCObject implements Comparable<PCEdge> {
 		}
 		return false;
 	}
-	
+
 	@Override
 	protected void runDelayedCommands() {
 		for (GraphicalRepresentation gr : allGr) {
 			if (commandsforGr.get(0) != null && !commandsforGr.get(0).isEmpty()) {
 				((GraphicalRepresentationEdge) gr)
-						.setWeight(((PCInteger) this.commandsforGr.get(0).pop())
+						.setWeight(((PCInteger) commandsforGr.get(0).pop())
+								.getLiteralValue());
+			}
+			if (commandsforGr.get(1) != null && !commandsforGr.get(1).isEmpty()) {
+				((GraphicalRepresentationEdge) gr)
+						.setColor(((PCString) commandsforGr.get(1).pop())
 								.getLiteralValue());
 			}
 		}
-		this.commandsforGr.get(0).clear();
+		commandsforGr.get(0).clear();
+		commandsforGr.get(1).clear();
+
 	}
 
 	public static PCEdge getNull() {
